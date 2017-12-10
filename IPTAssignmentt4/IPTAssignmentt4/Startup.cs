e4befd986448be4b6cd412d0048eff02163a0d7d
asadrizvi93 <32680417+asadrizvi93@users.noreply.github.com>
2017-12-11 00:46:42 +0500

    IPTAssignment4.v1
    a product table was added and mvc initially tested

---
+++ IPTAssignmentt4/IPTAssignmentt4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IPTAssignmentt4.Startup))]
+namespace IPTAssignmentt4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
